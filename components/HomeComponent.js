ccc6c292770d9b16739ca663837b12f0ce59cd16
daniel-peon <dgpeon62@gmail.com>
2021-04-10 13:05:44 -0400

    First Commit

---
+++ components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+
+class Home extends Component {
+  static navigationOptions = {
+    title: "All Your Base Games",
+  };
+  render() {
+    return <View />;
+  }
+}
+
+export default Home;
